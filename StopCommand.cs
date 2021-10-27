8bf3abfb6498c5f03fb590c010a4d30febe0fc9f
Andreea Rendes <andreearendes@outlook.com>
2021-10-27 19:14:36 +0300

    Laborator4

---
+++ StopCommand.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows.Input;
+
+
+namespace Rendes_Andreea_Lab2.CustomCommands
+{
+    class StopCommand
+    {
+        private static RoutedUICommand launch_command;
+
+        static StopCommand()
+        {
+            InputGestureCollection myInputGestures = new InputGestureCollection();
+            myInputGestures.Add(new KeyGesture(Key.S, ModifierKeys.Control));
+            launch_command = new RoutedUICommand("Launch", "Launch", typeof(StopCommand), myInputGestures);
+        }
+
+        public static RoutedUICommand Launch
+        {
+            get
+            {
+                return launch_command;
+            }
+        }
+    }
+}
